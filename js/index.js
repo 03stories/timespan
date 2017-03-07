19bfa650e0bb0f5d5bd6c1c44b13cb3f1d21a349
James Lytle <jlytle@gmail.com>
2017-03-06 23:41:08 -0500

    first scrappy pass at making a single camera active at a time.
    Since photos can overlap in time or come very close, only show the
    details for camera row that is hovered over.

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ let data = [
     title: 'Camera 1',
     items: [
       {
-        timestamp: 1450000001,
+        timestamp: 1440000000,
         type: 'photo',
         url: 'http://www.icelandprocruises.co.uk/media/img/gallery/home/0002-gallery-iceland-waterfall-1.jpg',
         title: 'landscape',
@@ -93,7 +93,7 @@ let data = [
     title: 'Camera 3',
     items: [
       {
-        timestamp: 1480340180,
+        timestamp: 1480110080,
         type: 'photo',
         url: 'http://cdn.images.express.co.uk/img/dynamic/133/590x/travel-activity-Iceland-Northern-Lights-Reykjavik-UploadExpress-Sophie-Donnelly-637734.jpg',
         title: 'lights',
@@ -118,8 +118,8 @@ let data = [
 ];
 
 let xScale = d3.scaleTime()
-          .domain([new Date(2016, 0, 1), new Date()]) // TODO: find min/max dates from data timestamps
-          .range([0, 700]); // 700px wide
+          .domain([new Date(2015, 0, 1), new Date()]) // TODO: find min/max dates from data timestamps
+          .range([0, 900]); // 700px wide
 
 let dateFormatter = d3.timeFormat('%b %e, %Y, %H:%M:%S');
 
@@ -131,10 +131,15 @@ var cameraEnter = container.selectAll('.camera').data(data)
   .enter()
     .append('div')
     .attr('class','camera')
+    .on('mouseover', function () {
+      d3.select(this).attr('class', 'camera active');
+    })
+    .on('mouseout', function () {
+      d3.select(this).attr('class', 'camera');
+    })
 ;
 cameraEnter.append('div').attr('class', 'camera-title');
 cameraEnter.append('div').attr('class', 'camera-items');
-
 
 // set title
 container.selectAll('.camera').selectAll('.camera-title').text( d => d.title );
@@ -177,20 +182,30 @@ d3.select('.timeline-guide').selectAll('.tick').data(xScale.domain())
 // add timeline slider
 let slider = d3.select('.timeline-slider');
 let guide = d3.select('.timeline-guide').node();
-let allItems =d3.selectAll('.camera-item');
+let allItems = d3.selectAll('.camera-item');
 d3.select('.timeline').on('mousemove', function() {
   // update slider position
   slider.style('left', d3.event.pageX + 'px');
+
+  // var activeCam = d3.select('.camera');
+  // activeCam.attr('class', 'camera active');
   
   // mark relevant cards active
   var xy = d3.mouse(guide);
   var dateX = xy[0];
   var date = xScale.invert(dateX);
   var mouseTimestamp = Math.floor(date.getTime()/1000);
-  allItems.classed('active', d =>  Math.abs(d.timestamp - mouseTimestamp) < 1000000);
+  allItems.classed('active', d =>  Math.abs(d.timestamp - mouseTimestamp) < 500000);
+
+  // mark relevant rows active
+  // var xy = d3.mouse(guide);
+  // var dateX = xy[0];
+  // var date = xScale.invert(dateX);
+  // var mouseTimestamp = Math.floor(date.getTime()/1000);
+  // allItems.classed('active', d =>  Math.abs(d.timestamp - mouseTimestamp) < 1000000);
   
   // update details with one active item's details
-  var active = d3.select('.camera-item.active');
+  var active = d3.select('.camera.active .camera-item.active');
   if (active.size() === 0) {
     d3.select('.timeline-date').text('');
     d3.select('.timeline-title').text('');
